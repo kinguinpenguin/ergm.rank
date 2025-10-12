e147c163afefde0242841d655eaafc1ce5effa32
kinguinpenguin <patdude102@gmail.com>
2025-10-13 02:00:08 +1100

    logratio calculation

--- src/wtMHproposals.c
+++ src/wtMHproposals.c
@@ -89,9 +89,9 @@ WtMH_P_FN(Mp_PartialDisc){
   Mweight[0] = proposal;
 
   if (edgestate == min || edgestate == max) {
-    MHp->logratio = +-log(0.5);
+    MHp->logratio = +log(0.5);
   }else if (proposal == min || proposal == max) {
-    MHp->logratio = +-log(0.5);
+    MHp->logratio = -log(0.5);
   } else {
     MHp->logratio = 0;
   }
