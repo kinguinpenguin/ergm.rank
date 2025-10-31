b33d481bbadf703b3abdb520d192dd6a99f131ad
kinguinpenguin <patdude102@gmail.com>
2025-10-31 12:01:20 +1100

    added NULL back to InitWtErgmProposal.R

--- R/InitWtErgmProposal.R
+++ R/InitWtErgmProposal.R
@@ -30,6 +30,7 @@ InitWtErgmProposal.AlterSwap <- function(arguments, nw) {
 #' @template ergmProposal-general
 #' @concept valued
 #' @concept ordinal
+NULL
 
 InitWtErgmProposal.AdjacentAlterSwap <- function(arguments, nw) {
   M <- arguments$constraints$ranking$M
